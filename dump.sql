93511c45297df8779a86f57dac05d4800cb09a23
Enes Işık <30756694+isikenes@users.noreply.github.com>
2023-05-24 13:07:26 +0300

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `lightoatm` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `lightoatm`;
 -- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: lightoatm
@@ -78,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-24 12:43:15
+-- Dump completed on 2023-05-24 13:05:31
